10a226ab4812024d3600fb43d97edc8a7253d939
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-08-27 20:37:26 +0300

    Initial commit

---
+++ emoji-critic/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// Добавили компонент Header с логотипом проекта
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <Header />
+      </div>
+  );
+}
+
+export default App;
